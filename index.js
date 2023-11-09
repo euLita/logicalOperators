e56be5fe8ddbcc77a34730fb5c164d6c93c36a8c
iLita <11500712-iLita@users.noreply.replit.com>
2023-11-09 23:42:48 +0000

    using of the logical operators

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 /*Operadores logicos.
 Menores de 18 -> Menor de idade (meia).
+Maiores de 60 anos -> Idosos (meia)
 Entre 18 e 60 -> Adultos (meia apenas com carteirinha).
-Maiores de 60 anos -> Idosos (meia).
 
 INTEIRA: Adulto e NÃO tem carteirinha.
 MEIA: todo o resto.
 */
 
-let age = 19;
-let isCarteirinha = false;
-
-  if (age < 18 && age >= 60 && isCarteirinha) {
-    console.log('meia')
-  } else if (age >= 18 && age <= 60 && isCarteirinha) {
-    console.log('meia')
-  } else {
-    console.log('inteira')
-  }
+let age = prompt("Informe uma idade: ");
+let isCarteirinha = prompt("Tem carteirinha: ");
+if (age < 18 || age > 60 || isCarteirinha == "SIM") {
+  console.log("meia");
+} else {
+  console.log("inteira")
+}
